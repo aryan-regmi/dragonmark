fd7d8fc3a48bbe0b22dfbf79de2c18c5f9dd67ef
Aryan Regmi <aryan_regmi@protonmail.com>
2024-03-02 01:42:46 -0700

    Got overlay working

---
+++ src/lib/races/Dragonborn.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let toggleOverlay: boolean;
+  function close() {
+    toggleOverlay = !toggleOverlay;
+  }
+</script>
+
+{#if toggleOverlay}
+  <div class="overlay">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit tenetur
+    quia ratione eligendi odio dicta labore quo magni facilis nostrum, non totam
+    vel accusamus eos deleniti omnis eius laboriosam officia!
+    <button on:click|preventDefault|stopPropagation={close}>Close</button>
+  </div>
+{/if}
+
+<style>
+  .overlay {
+    position: fixed;
+    display: block;
+    width: 80%;
+    height: 100%;
+    left: 10%;
+    background-color: black;
+    opacity: 95%;
+    z-index: 2;
+    cursor: pointer;
+    overflow-y: scroll;
+  }
+</style>
--- src/lib/races/RaceMain.svelte
+++ src/lib/races/RaceMain.svelte
@@ -1,101 +1,106 @@
 <script lang="ts">
-  const races = [
-    "Dragonborn",
-    "Dwarf",
-    "Elf",
-    "Gnome",
-    "Half-Elf",
-    "Half-Orc",
-    "Halfling",
-    "Human",
-    "Tiefling",
-    "Custom",
-    "Custom",
-    "Aarakocra",
-    "Aasimar",
-    "Changeling",
-    "Deep",
-    "Gnome",
-    "Duergar",
-    "Eladrin",
-    "Fairy",
-    "Firbolg",
-    "Genasi (Air)",
-    "Genasi (Earth)",
-    "Genasi (Fire)",
-    "Genasi (Water)",
-    "Githyanki",
-    "Githzerai",
-    "Goliath",
-    "Harengon",
-    "Kenku",
-    "Locathah",
-    "Owlin",
-    "Satyr",
-    "Sea Elf",
-    "Shadar-Kai",
-    "Tabaxi",
-    "Tortle",
-    "Triton",
-    "Verdan",
-    "Bugbear",
-    "Centaur",
-    "Goblin",
-    "Grung",
-    "Hobgoblin",
-    "Kobold",
-    "Lizardfolk",
-    "Minotaur",
-    "Orc",
-    "Shifter",
-    "Yuan-Ti",
-    "Kender",
-    "Kalashtar",
-    "Warforged",
-    "Aetherborn",
-    "Aven",
-    "Khenra",
-    "Kor",
-    "Merfolk",
-    "Naga",
-    "Siren",
-    "Vampire",
-    "Dhamphir",
-    "Hexblood",
-    "Reborn",
-    "Loxodon",
-    "Simic Hybrid",
-    "Vedalken",
-    "Astral Elf",
-    "Autognome",
-    "Hadozee",
-    "Thri-kreen",
-    "Leonin",
-    "Giff",
-    "Glitchling",
-    "Owlfolk",
-    "Plasmoid",
-    "Rabbitfolk",
-    "Revenant",
-    "Viashino",
-  ].sort();
+  import Dragonborn from "./Dragonborn.svelte";
+
+  const races: { [index: string]: { data: any; visible: boolean } } = {
+    Dragonborn: { data: Dragonborn, visible: false },
+    Dwarf: { data: null, visible: false },
+    Elf: { data: null, visible: false },
+    Gnome: { data: null, visible: false },
+    "Half-Elf": { data: null, visible: false },
+    "Half-Orc": { data: null, visible: false },
+    Halfling: { data: null, visible: false },
+    Human: { data: null, visible: false },
+    Tiefling: { data: null, visible: false },
+    Custom: { data: null, visible: false },
+    Aarakocra: { data: null, visible: false },
+    Aasimar: { data: null, visible: false },
+    Changeling: { data: null, visible: false },
+    Deep: { data: null, visible: false },
+    Duergar: { data: null, visible: false },
+    Eladrin: { data: null, visible: false },
+    Fairy: { data: null, visible: false },
+    Firbolg: { data: null, visible: false },
+    "Genasi (Air)": { data: null, visible: false },
+    "Genasi (Earth)": { data: null, visible: false },
+    "Genasi (Fire)": { data: null, visible: false },
+    "Genasi (Water)": { data: null, visible: false },
+    Githyanki: { data: null, visible: false },
+    Githzerai: { data: null, visible: false },
+    Goliath: { data: null, visible: false },
+    Harengon: { data: null, visible: false },
+    Kenku: { data: null, visible: false },
+    Locathah: { data: null, visible: false },
+    Owlin: { data: null, visible: false },
+    Satyr: { data: null, visible: false },
+    "Sea Elf": { data: null, visible: false },
+    "Shadar-Kai": { data: null, visible: false },
+    Tabaxi: { data: null, visible: false },
+    Tortle: { data: null, visible: false },
+    Triton: { data: null, visible: false },
+    Verdan: { data: null, visible: false },
+    Bugbear: { data: null, visible: false },
+    Centaur: { data: null, visible: false },
+    Goblin: { data: null, visible: false },
+    Grung: { data: null, visible: false },
+    Hobgoblin: { data: null, visible: false },
+    Kobold: { data: null, visible: false },
+    Lizardfolk: { data: null, visible: false },
+    Minotaur: { data: null, visible: false },
+    Orc: { data: null, visible: false },
+    Shifter: { data: null, visible: false },
+    "Yuan-Ti": { data: null, visible: false },
+    Kender: { data: null, visible: false },
+    Kalashtar: { data: null, visible: false },
+    Warforged: { data: null, visible: false },
+    Aetherborn: { data: null, visible: false },
+    Aven: { data: null, visible: false },
+    Khenra: { data: null, visible: false },
+    Kor: { data: null, visible: false },
+    Merfolk: { data: null, visible: false },
+    Naga: { data: null, visible: false },
+    Siren: { data: null, visible: false },
+    Vampire: { data: null, visible: false },
+    Dhamphir: { data: null, visible: false },
+    Hexblood: { data: null, visible: false },
+    Reborn: { data: null, visible: false },
+    Loxodon: { data: null, visible: false },
+    "Simic Hybrid": { data: null, visible: false },
+    Vedalken: { data: null, visible: false },
+    "Astral Elf": { data: null, visible: false },
+    Autognome: { data: null, visible: false },
+    Hadozee: { data: null, visible: false },
+    "Thri-kreen": { data: null, visible: false },
+    Leonin: { data: null, visible: false },
+    Giff: { data: null, visible: false },
+    Glitchling: { data: null, visible: false },
+    Owlfolk: { data: null, visible: false },
+    Plasmoid: { data: null, visible: false },
+    Rabbitfolk: { data: null, visible: false },
+    Revenant: { data: null, visible: false },
+    Viashino: { data: null, visible: false },
+  };
 
   export let chosenRace = "";
 
+  let overlay = true;
   function raceInfo(this: any) {
-    chosenRace = this.id;
-    // TODO: Open info for the clicked race
-    // (make it permanantly chang color if chosen)
+    races[this.id].visible = true;
+    overlay = races[this.id].visible;
   }
 </script>
 
 <ul class="races">
-  {#each races as race}
+  {#each Object.entries(races) as [race, data]}
     <li class="race-item">
       <button class="race-btn" id={race} on:click={raceInfo}
         ><strong>{race}</strong></button
       >
     </li>
+
+    {#if data.visible}
+      <svelte:component this={data.data} bind:toggleOverlay={data.visible}
+      ></svelte:component>
+    {/if}
   {/each}
 </ul>
 
@@ -117,8 +122,7 @@
   li {
     display: inline;
     vertical-align: top;
-    padding-right: 1.5vw;
-    padding-left: 0.1vw;
+    padding-right: 1.7vw;
   }
 
   .race-btn {

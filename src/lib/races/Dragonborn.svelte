45d8c396ffd3c529e4bf4af802ef8c26ebaaed6a
Aryan Regmi <aryan_regmi@protonmail.com>
2024-03-03 07:38:42 -0700

    Added Overlay component

--- src/lib/races/Dragonborn.svelte
+++ src/lib/races/Dragonborn.svelte
@@ -1,39 +1,37 @@
 <script lang="ts">
-  export let toggleOverlay: boolean;
-  function close() {
-    toggleOverlay = !toggleOverlay;
-  }
+  import Overlay from "./Overlay.svelte";
 
+  export let toggleOverlay: boolean;
   export let chosen = "";
-  function choose() {
-    chosen = "Dragonborn";
-    close();
-  }
 </script>
 
-{#if toggleOverlay}
-  <div class="overlay">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit tenetur
-    quia ratione eligendi odio dicta labore quo magni facilis nostrum, non totam
-    vel accusamus eos deleniti omnis eius laboriosam officia!
-    <div>
-      <button on:click|preventDefault|stopPropagation={choose}>Choose</button>
-      <button on:click|preventDefault|stopPropagation={close}>Close</button>
-    </div>
+<Overlay bind:toggleOverlay bind:chosen>
+  <div>
+    <h2>Dragonborn</h2>
+    <p id="summary">
+      <strong>
+        Born of dragons, as their name proclaims, the dragonborn walk proudly
+        through a world that greets them with fearful incomprehension. Shaped by
+        draconic gods or the dragons themselves, dragonborn originally hatched
+        from dragon eggs as a unique race, combining the best attributes of
+        dragons and humanoids. Some dragonborn are faithful servants to true
+        dragons, others form the ranks of soldiers in great wars, and still
+        others find themselves adrift, with no clear calling in life.
+      </strong>
+    </p>
   </div>
-{/if}
+  <ul>
+    <li>
+      <strong>Ability Score Increase.</strong> Your Strength score increases by 2,
+      and your Charisma score increases by 1.
+    </li>
+  </ul>
+</Overlay>
 
 <style>
-  .overlay {
-    position: fixed;
-    display: block;
-    width: 80%;
-    height: 100%;
-    left: 10%;
-    background-color: black;
-    opacity: 95%;
-    z-index: 2;
-    cursor: pointer;
-    overflow-y: scroll;
+  #summary {
+    padding-left: 2vw;
+    padding-right: 2vw;
+    font-size: 16px;
   }
 </style>
---
+++ src/lib/races/Overlay.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let toggleOverlay: boolean;
+  function close() {
+    toggleOverlay = !toggleOverlay;
+  }
+
+  export let chosen = "";
+  function choose() {
+    chosen = "Dragonborn";
+    close();
+  }
+</script>
+
+{#if toggleOverlay}
+  <div class="overlay">
+    <div>
+      <slot />
+    </div>
+    <div>
+      <button on:click|preventDefault|stopPropagation={choose}>Choose</button>
+      <button on:click|preventDefault|stopPropagation={close}>Close</button>
+    </div>
+  </div>
+{/if}
+
+<style>
+  .overlay {
+    position: fixed;
+    display: block;
+    width: 80%;
+    height: 100%;
+    left: 10%;
+    background-color: #3d3d29;
+    opacity: 98%;
+    z-index: 2;
+    cursor: pointer;
+    overflow-y: scroll;
+  }
+</style>
